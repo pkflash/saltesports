e5f406bcd1fec8cd70dcb8165ce38ec5b765b695
LonelyMatt <132096155+LonelyMatt@users.noreply.github.com>
2025-01-12 23:39:14 -0800

    fixed the home

--- salt/urls.py
+++ salt/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = "salt_app"
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("home", views.home, name="home"),
     path("about", views.about, name="about"),
     path("content", views.content, name="content"),
     path("gallery", views.gallery, name="gallery"),
