6b2dd72f418926898e72fa73fdcff01ced26d429
LonelyMatt <132096155+LonelyMatt@users.noreply.github.com>
2025-04-03 01:06:23 -0700

    k

--- salt/admin.py
+++ salt/admin.py
@@ -4,6 +4,6 @@ from .models import Member
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
     list_display = ('username', 'full_name', 'is_active', 'order')
-    list_filter = ('is_active', 'roles')
+    list_filter = ('is_active',)
     search_fields = ('username', 'full_name')
     ordering = ('order', 'username')
